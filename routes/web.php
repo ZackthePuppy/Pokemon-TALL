17291a217d0da22f835f25c9e31d95300d3024e1
Mayk Lemuel <mayk@nystealth.com>
2026-02-18 16:35:25 -0500

    added nature page

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')->name('home');
+Route::view('/generation', 'generation')->name('generation');
+Route::view('/nature', 'nature')->name('nature');
